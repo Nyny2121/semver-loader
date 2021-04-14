f2afec07abeac87fcae5864412fa96df9eac40ea
Nyny2121 <nyny830@gmail.com>
2021-04-14 16:52:52 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-$builds = '/var/www/example.com/public_html/builds/';
+$builds = '/var/www/example.com/public_html/builds/';		# Change this to reflect your build directory on the server
 $folders = array_diff(scandir($builds,1), array('.','..'));
 $latest = '0.0.0';
 
@@ -21,6 +21,7 @@ foreach ($folders as $str) {
 }
 
 echo '<body style="margin: 0; padding: 0">';
+# Change the following line to the link to your builds folder URL
 echo ('<iframe src="https://example.com/builds/'.$latest.'/index.html" style="border: 0; width: 100%; height: 100%;" name="game-iframe" autofocus="true"></iframe>');
 echo '</body>'
 ?>
