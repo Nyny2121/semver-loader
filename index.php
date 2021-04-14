67111774dac40008815605c00f07158be1658e86
Nyny2121 <nyny830@gmail.com>
2021-04-14 16:41:19 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+$builds = '/var/www/example.com/public_html/builds/';
+$folders = array_diff(scandir($builds,1), array('.','..'));
+$latest = '0.0.0';
+
+foreach ($folders as $str) {
+	$version = explode('.',$str);
+	$latest_arr = explode('.', $latest);
+
+	if ($latest_arr[0] < $version[0]) {
+		$latest = $str;
+	} elseif ($latest_arr[0] == $version[0]) {
+		if ($latest_arr[1] < $version[1]) {
+			$latest = $str;
+		} elseif ($latest_arr[1] == $version[1]) {
+			if ($latest_arr[2] < $version[2]) {
+				$latest = $str;
+			}
+		}
+	}
+}
+
+echo '<body style="margin: 0; padding: 0">';
+echo ('<iframe src="https://example.com/builds/'.$latest.'/index.html" style="border: 0; width: 100%; height: 100%;" name="game-iframe" autofocus="true"></iframe>');
+echo '</body>'
+?>
